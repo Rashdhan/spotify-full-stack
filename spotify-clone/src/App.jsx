0ef0a319d6e51f2e178cd8ed178a9f19d454bc8f
Your Name <youremail@example.com>
2025-03-14 11:06:33 +0530

    Updated files with recent changes

--- spotify-clone/src/App.jsx
+++ spotify-clone/src/App.jsx
@@ -1,26 +1,30 @@
-import React, { useContext } from 'react';
-import Sidebar from './components/Sidebar';
-import Player from './components/Player';
-import Display from './components/Display';
-import { PlayerContext } from './context/PlayerContext';
+import React, { useContext } from 'react'
+import Sidebar from './components/Sidebar'
+import Player from './components/Player'
+import Display from './components/Display'
+import { PlayerContext } from './context/PlayerContext'
 
 const App = () => {
-  const { audioRef, track, songsData } = useContext(PlayerContext);
 
-  if (!songsData.length) {
-    return <div>Loading...</div>; // Only show loading if songsData is empty
-  }
+  const { audioRef, track, songsData } = useContext(PlayerContext);
 
   return (
     <div className='h-screen bg-black'>
-      <div className='h-[90%] flex'>
-        <Sidebar />
-        <Display />
-      </div>
-      <Player />
-      {track && <audio ref={audioRef} src={track.file} preload='auto'></audio>}
-    </div>
-  );
-};
+      {
+        songsData.length !==0
+          ? <>
+            <div className='h-[90%] flex'>
+              <Sidebar />
+              <Display />
+            </div>
+            <Player />
+          </>
+          : ""
+      }
 
-export default App;
+      <audio ref={audioRef} src={track?track.file:""} preload='auto'></audio>
+    </div>
+  )
+}
+
+export default App
